5b8e4fbe45ce9d5c765618c32d211a833a3ffee9
TheUncleLake <100882470+TheUncleLake@users.noreply.github.com>
2024-03-30 22:27:31 -0400

    some minor progress website

---
+++ src/main/java/org/ehotel/ConnectionDB.java
@@ -0,0 +1,51 @@
+package org.ehotel;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private Connection con = null;
+
+    /**
+     * Establishes a connection with the database, initializes and returns
+     * the Connection object.
+     *
+     * @return Connection, the Connection object
+     * @throws Exception if an error occurs when connecting to the database
+     */
+    public Connection getConnection() throws Exception {
+        /* Database connection settings, change dbName, dbusername, dbpassword */
+        final String ipAddress = "127.0.0.1";
+        final String dbServerPort = "5432";
+        final String dbName = "postgres";
+        final String dbusername = "postgres";
+        final String dbpassword = "";
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://"
+                    + ipAddress + ":" + dbServerPort + "/" + dbName, dbusername, dbpassword);
+            return con;
+        } catch (Exception e) {
+            throw new Exception("Could not establish connection with the Database Server: "
+                    + e.getMessage());
+        }
+    }
+
+    /**
+     * Close database connection. It is very important to close the database connection
+     * after it is used.
+     *
+     * @throws SQLException if it fails to close the connection
+     */
+    public void close() throws SQLException {
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+            throw new SQLException("Could not close connection with the Database Server: "
+                    + e.getMessage());
+        }
+
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,16 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> e-Hotels - America's convenient hotel service </title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+
 <body>
-<h2>Hello World!</h2>
+    <h2>Hello World!</h2>
 </body>
+
 </html>
