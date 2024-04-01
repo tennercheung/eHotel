243308c15f3715f87eacb22d775b84d217b7d9d6
Harry240-haha <61801455+Harry240-haha@users.noreply.github.com>
2024-03-31 22:07:10 -0400

    Update ehotel-insert-data.sql

--- ehotel-insert-data.sql
+++ ehotel-insert-data.sql
@@ -1,15 +1,15 @@
 SET search_path = "ehotel";
 
 INSERT INTO HotelChain (ChainID,PhoneNum,Email,CentralOfficeAddr,NumHotels) VALUES
-(1202,1234567890,'info@hilton.com','123 Main St, Cityville, NY 12345',12);
+(1202,1234567890,'info@hilton.com','123 Main St, Cityville, NY 12345',8);
 INSERT INTO HotelChain (ChainID,PhoneNum,Email,CentralOfficeAddr,NumHotels) VALUES
-(3104,2049184029,'info@haptoninn.com','92 Jersey Rd, Newtown, NJ 42230',11);
+(3104,2049184029,'info@haptoninn.com','92 Jersey Rd, Newtown, NJ 42230',8);
 INSERT INTO HotelChain (ChainID,PhoneNum,Email,CentralOfficeAddr,NumHotels) VALUES
 (9193,1233920394,'info@sheratonhotels.com','2 Fox St, New York City, NY 12324',8);
 INSERT INTO HotelChain (ChainID,PhoneNum,Email,CentralOfficeAddr,NumHotels) VALUES
-(7524,6139302583,'info@deltahotels.com','55 Sunny Trail, San Francisco, CA 02331',9);
+(7524,6139302583,'info@deltahotels.com','55 Sunny Trail, San Francisco, CA 02331',8);
 INSERT INTO HotelChain (ChainID,PhoneNum,Email,CentralOfficeAddr,NumHotels) VALUES
-(2049,4129329048,'info@marriotthotels.com','3294 Main St, South County, OH 76205',10);
+(2049,4129329048,'info@marriotthotels.com','3294 Main St, South County, OH 76205',8);
 
 
 
@@ -3359,17 +3359,14 @@ INSERT INTO RoomAmenity(RoomNum,HotelID,Amenity) VALUES
 
 
 
-
-
-
-
-
-
-
-
-
-
 SELECT * from RoomAmenity;
+
+
+
+
+
+
+
 
 DELETE from Employee;
 INSERT INTO Employee(EmployeeSIN,PositionName,FullName,Addr,NumHotels) VALUES
@@ -3378,25 +3375,60 @@ INSERT INTO Employee(EmployeeSIN,PositionName,FullName,Addr,NumHotels) VALUES
 (876678565,'Concierge','Jane Robbins','2760 Iger St, New Braunfels, TX 78130',1);
 
 
-SELECT * from Employee;
 
+
+
+
+
+
+SELECT * from Employee;
 INSERT INTO Renting(RentingID,HotelID,RentingDate,RoomNum,EmployeeSIN) VALUES
 (20220, 230284,'January 20, 2024',310,123321111);
 
-SELECT * from Renting;
 
+
+
+
+
+
+
+SELECT * from Renting;
 INSERT INTO Manager(ManagerID,HotelID) VALUES
 (230444220,230284);
 
-SELECT * from Manager;
 
+
+
+
+
+
+
+
+SELECT * from Manager;
 INSERT INTO Customer(CustomerID,Addr,RegistrationDate,IDType,FullName,EmployeeSIN) VALUES
 (310322,'598 TX St, Lake Jackson, TX 77566','January 19, 2024', 'Driver''s License', 'Jack Jones',876678565);
 
-SELECT * from Customer;
 
+
+
+
+
+
+
+SELECT * from Customer;
 INSERT INTO Booking(BookingID,HotelID,RoomNum,BookingDate,CheckInDate,CustomerID,EmployeeSIN) VALUES
 (3029489,230284,310,'January 9, 2024','January 19, 2024',310322,876678565);
+
+
+
+
+
+
+
+
+
+
+
 
 SELECT * from Booking;
 
